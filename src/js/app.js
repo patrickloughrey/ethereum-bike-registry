465297448bcaafc1f19982cb62dd57ed9890349c
Patrick Loughrey <pgl19@scarletmail.rutgers.edu>
2018-10-23 20:55:16 -0400

    Finished register bike modal form and started javascript - solidity integration

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,11 @@
+/* Initialize variables */
+var name = $('#name');
+var color = $('#color');
+var wheels = $('#wheels');
+var brakes = $('#brakes');
+var basket = $('#basket');
+
+/* JavaScript Smart Contract logic */
 App = {
   web3Provider: null,
   contracts: {},
@@ -20,16 +28,36 @@ App = {
 
   initContract: function() {
     $.getJSON('BikeRegistry.json', function(data) {
-      // Get the necessary contract artifact file and instantiate it with truffle-contract.
+      /* Get the necessary contract artifact file and instantiate it with truffle-contract */
       var TutorialTokenArtifact = data;
       App.contracts.TutorialToken = TruffleContract(TutorialTokenArtifact);
 
-      // Set the provider for our contract.
+      /* Set the provider for our contract */
       App.contracts.TutorialToken.setProvider(App.web3Provider);
-
     });
 
     return App.bindEvents();
+  },
+
+  bindEvents: function() {
+    $(document).on('click', '#registerBike', App.registerBike);
+  },
+
+  registerBike: function(event) {
+    event.preventDefault();
+
+    var BikeInstance;
+
+    App.contracts.BikeRegistry.deployed().then(function(instance) {
+      console.log(instance);
+      BikeInstance = instance;
+
+      return BikeInstance.registerBike(name.val(), color.val(), wheels.val(), brakes.val(), basket.val());
+
+    }).then(function(result) {
+        console.log('Contract has been deployed!\n')
+    }
+
   }
 
 };
