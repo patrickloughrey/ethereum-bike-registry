8418c884a0b8dae5c5ecf7ec8c35d13d3b10a9c5
Patrick Loughrey <pgl19@scarletmail.rutgers.edu>
2018-10-26 17:15:58 -0400

    Added modals for each button and styling

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,40 @@
+/* Function to dynamically add transaction to DOM */
+function addRegisteredBike(obj, name) {
+
+    /* Close Modal */
+    $('.btn-close').click();
+
+    /* Get Date & Time */
+    var date = new Date();
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var month = months[date.getMonth()];
+    var year = date.getFullYear();
+    var day = date.getDate();
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
+    var timeStamp = month + ", " + day + ", " + year;
+
+    contractAddr = obj.receipt.contractAddress;
+    console.log(obj);
+
+    /* Append to Transactions */
+    var transactionsDiv = $('#transactions');
+    var column = $('<div>').addClass('col-md-6');
+    var offset = $('<div>').addClass('col-md-4 offset-md-4');
+
+    addrVal = $('<p>').text(contractAddr);
+    nameVal = $('<p>').text(name).addClass('name-col-md-6');
+    timeVal = $('<p>').text(timeStamp).addClass('time-col-md-6');
+
+    column.append(addrVal);
+    offset.append(timeVal);
+    offset.append(nameVal);
+
+    /* Append transaction details to div */
+    transactionsDiv.append(column);
+    transactionsDiv.append(offset);
+}
+
 /* JavaScript Smart Contract logic */
 App = {
   web3Provider: null,
@@ -44,14 +81,16 @@ App = {
     var BikeInstance;
 
     App.contracts.BikeRegistry.deployed().then(function(instance) {
-      // console.log(instance);
+      console.log(instance);
       BikeInstance = instance;
 
       return BikeInstance.registerBike($('#name').val(), $('#color').val(), $('#wheels').val(), $('#brakes').val(), $('#basket').val());
 
     }).then(function(result) {
-        console.log(result);
+
         console.log('Contract has been deployed!\n');
+
+        addRegisteredBike(result, $('#name').val());
 
     }).catch(function(err) {
         console.log(err)
